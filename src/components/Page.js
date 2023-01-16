3b875020019e9467c0f00f9cad67e8a659f03445
Prince-Deka <princedeka00@gmail.com>
2023-01-16 13:32:58 +0530

    updated the App.css file

---
+++ src/components/Page.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Page.css"
+function Page() {
+    return(
+        <>
+            <div className="parent">
+                <div className="innerDiv"><h1>Div 1</h1></div>
+                <div className="innerDiv"><h1>Div 2</h1></div>
+                <div className="innerDiv"><h1>Div 2</h1></div>
+            </div>
+        
+        </>
+    );
+}
+
+export default Page;
